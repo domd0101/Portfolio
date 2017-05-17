0fe0392c882e409db5d14a8478f4da27e8bbb65a
domd0101 <ddecicco@buffalo.edu>
2017-05-17 13:59:56 -0600

    k

---
+++ jobstuff.js
@@ -0,0 +1,7 @@
+1: optimize codepen profile (doneish)
+2: linkedin optimize (doneish)
+3: resume optimize (doneish)
+4: host projects (maps done)
+5: finish portfolio / host it
+6: apply jobs
+7: connect on linkedin
